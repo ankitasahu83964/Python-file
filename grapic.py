b64aca15a217d689559268fb3292ba2dc06edbca
Ankita sahu <ankitasahu83964@gmail.com>
2025-12-04 23:13:33 +0530

    Add files via upload

---
+++ grapic.py
@@ -0,0 +1,19 @@
+
+import turtle
+t = turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+turtle.done()
+
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+turtle.done()
